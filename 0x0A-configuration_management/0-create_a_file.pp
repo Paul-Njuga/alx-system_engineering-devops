efd00b62a855461d8d6aada5453ad4e2a2b40cb9
Paul Njuga <paulnjuga@gmail.com>
2023-06-24 00:42:35 +0300

    Fix: Rmv double quotes & align arrows

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Create a file in /tmp
 
-file { "/tmp/school":
-    ensure => "present",
-    owner => "www-data",
-    group => "www-data",
-    mode => "0744",
-    content => "I love Puppet",
+file { '/tmp/school':
+    ensure  => 'present',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
 }
