054f49a0ee6b91448a2a8dbc3b6dc43e081201bc
Paul Njuga <paulnjuga@gmail.com>
2023-06-24 01:06:18 +0300

    Add kill process puppet file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Install flask from pip3
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'pkill killmenow':
+  path   => 'usr/bin',
+  onlyif => 'usr/bin/pgrep killmenow',
 }
