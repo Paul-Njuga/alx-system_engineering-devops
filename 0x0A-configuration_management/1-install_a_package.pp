1c8fb9a907c1cf7e6e81e4911cef9990f067d7e1
Paul Njuga <paulnjuga@gmail.com>
2023-06-24 01:15:27 +0300

    Fix: Edited in place instead of coping

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Install flask from pip3
 
-exec { 'pkill killmenow':
-  path   => 'usr/bin',
-  onlyif => 'usr/bin/pgrep killmenow',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+# Kills a process named killmenow
+
+exec { 'pkill killmenow':
+  path   => 'usr/bin',
+  onlyif => 'usr/bin/pgrep killmenow',
+}
