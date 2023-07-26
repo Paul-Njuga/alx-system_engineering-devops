726feec9f430aa80989b75d61e6c73e2325f93b6
Paul Njuga <paulnjuga@gmail.com>
2023-07-27 01:47:49 +0300

    Fix: rmv newline backslash
    Backslash messes with get request

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,16 +7,15 @@ if __name__ == "__main__":
     from sys import argv
 
     usr_Id = argv[1]
-    usr = requests.get("https://jsonplaceholder.typicode.com/users/{}\
-                                ".format(usr_Id)).json()
-    tdlst = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}\
-                                  ".format(usr_Id)).json()
-
-    username = usr["username"]
+    usr = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                       format(usr_Id)).json()
+    tds = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                       format(usr_Id)).json()
+    username = usr.get("username")
 
     with open(usr_Id + ".csv", "w") as f:
         writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-        for td in tdlst:
-            status = td["completed"]
-            title = td["title"]
+        for td in tds:
+            status = td.get("completed")
+            title = td.get("title")
             writer.writerow([usr_Id, username, status, title])
