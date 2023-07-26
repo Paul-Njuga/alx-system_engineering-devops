8565f5b36cd207613fa8e41c272c7421b39ca780
Paul Njuga <paulnjuga@gmail.com>
2023-07-27 00:53:09 +0300

    Fix: correct csv filename

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -11,16 +11,13 @@ if __name__ == "__main__":
     usrs = usrs_response.json()
     tdlst = tdlst_response.json()
     emp_Id = argv[1]
-    emp_name = None
-    title = None
-    status = None
 
     if emp_Id.isdigit():
         for usr in usrs:
             if usr["id"] == int(emp_Id):
                 emp_name = usr["name"]
 
-        with open("FILE.csv", "w") as f:
+        with open("USER_ID.csv", "w") as f:
             writer = csv.writer(f, quoting=csv.QUOTE_ALL)
             for td in tdlst:
                 if td["userId"] == int(emp_Id):
