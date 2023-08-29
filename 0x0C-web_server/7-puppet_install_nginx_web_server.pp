165e139515bd4318e98eca468d11c314d8b0f10a
Paul Njuga <paulnjuga@gmail.com>
2023-08-29 23:22:47 +0300

    Edit for puppet lint

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -9,8 +9,8 @@ file { '/var/www/html/index.nginx-debian.html':
 }
 
 file_line { 'Add redirection, 301':
-  path   => '/etc/nginx/sites-available/default',
   ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
   after  => 'server_name _;',
   line   => '\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
